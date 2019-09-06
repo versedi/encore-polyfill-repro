b16893d105be18ebcef5b82bd2f54a011b501a2b
versedi <versedi@gmail.com>
2019-09-06 15:30:36 +0200

    Fix sentry path

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 //
 Encore.addPlugin(
     new SentryWebpackPlugin({
-        include: './public/assets/',
+        include: './build/',
         ignoreFile: '.sentrycliignore',
         ignore: [
             'node_modules',
@@ -25,10 +25,38 @@ Encore.setOutputPath('build/') // directory where compiled assets will be stored
 
 Encore.cleanupOutputBeforeBuild();
 
-
+Encore.configureBabel(
+    babelConfig => {
+        // add additional presets (preset-env is added by default)
+        babelConfig.presets.push('@babel/preset-flow');
+        // IE11/Edge requires below plugins
+        babelConfig.plugins.push('@babel/plugin-transform-object-assign');
+        babelConfig.plugins.push('@babel/plugin-transform-spread');
+        babelConfig.plugins.push('@babel/plugin-transform-exponentiation-operator');
+        babelConfig.plugins.push('@babel/plugin-transform-arrow-functions');
+        // no plugins are added by default, but you can add some
+        // babelConfig.plugins.push('styled-jsx/babel');
+        // if (Encore.isProduction()) {
+        //     babelConfig.plugins.push("transform-remove-console");
+        // }
+    },
+    {
+        // node_modules is not processed through Babel by default
+        // but you can whitelist specific modules to process
+        includeNodeModules: [
+            '@sentry/browser',
+            '@sentry/browser/esm',
+        ],
+        useBuiltIns: 'usage',
+        corejs: {
+            version: 3.2,
+            proposals: true,
+        },
+    },
+)
 
 Encore.splitEntryChunks()
-    .addEntry('app', './js/app.js')
+    .addEntry('app', './js/app.ts')
     .enableVersioning(Encore.isProduction())
     .enableIntegrityHashes(Encore.isProduction())
     .enableSingleRuntimeChunk()
